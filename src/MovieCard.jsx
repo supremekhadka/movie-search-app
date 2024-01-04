10a84a271a72d12041236e00b50c119bce8bf6b7
Supreme Khadka <supremekhadka2@gmail.com>
2024-01-04 17:03:47 +0545

    resolved image error issue

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './MovieCard.css'
 
 const MovieCard = ({ movie }) => {
+
+    const [ imgSrc, setImgSrc ] = useState();
+
+    useEffect(() => {
+        setImgSrc(movie.Poster);
+    }, [movie.Poster])
 
     return(
         <div className='movie'>
             <div>
                 <div>
-                    <img src={((movie.Poster !== 'N/A') && (movie.Poster.Response !== '404'))? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
+                    <img 
+                        src={imgSrc} 
+                        onError={() => {
+                            setImgSrc("https://via.placeholder.com/400");
+                        }}
+                        alt={movie.Title} />
                 </div>
                 <div>
                     <div>
