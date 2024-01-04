fb6f3d3772333c9a0043f11511543da0143d7838
Supreme Khadka <supremekhadka2@gmail.com>
2024-01-04 14:58:06 +0545

    updated style

--- src/App.js
+++ src/App.js
@@ -24,13 +24,18 @@ const App = () => {
     return(
         <div className='App'>
             <h1>Movie App</h1>
-            <p>Find your movie</p>
+            <pre>F i n d    y o u r    m o v i e</pre>
 
             <div className='search'>
                 <input
                     placeholder='Search for a movie'
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={(key) => {
+                        if(key.keyCode === 13){
+                            searchMovies(searchTerm);
+                        }
+                    }}
                 />
                 <img
                     src={SearchIcon}
@@ -38,22 +43,25 @@ const App = () => {
                     onClick={() => searchMovies(searchTerm)}
                 />
 
-                {
-                    movies?.length > 0 ? (
-                        <div className='container'>
-                            {movies.map((movie) => (
-                                <MovieCard movie = {movie} />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className='movies-not-found'>
-                            <h2>Movie not found.</h2>
-                        </div>
-                    )
-                }
+            </div>
+            {
+                movies?.length > 0 ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie = {movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='movies-not-found'>
+                        <h2>Movie not found.</h2>
+                    </div>
+                )
+            }
+
+            <div className="footer">
+                <span>Made by <a href='https://www.supremekhadka.com.np' style={{textDecoration: "none", fontWeight: 400, color: "#fff"}}>Supreme</a></span>
             </div>
 
-            
         </div>
     );
 }
