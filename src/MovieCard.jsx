7485ebf9dd8af9727233582acc5953114699988c
Raman Shakya <raman.gwarko15@gmail.com>
2024-01-04 19:39:46 +0545

    default image fix

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MovieCard.css'
 
 const MovieCard = ({ movie }) => {
+    const [imgSrc, setImgSrc] = useState();
+
+    useEffect(() => {
+        setImgSrc(movie.Poster);
+    }, [movie.Poster]);
 
     return(
         <div className='movie'>
             <div>
                 <div>
-                    <img src={((movie.Poster !== 'N/A') && (movie.Poster.Response !== '404'))? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
+                    <img 
+                        // src={((movie.Poster !== 'N/A') && (movie.Poster.Response !== '404'))? movie.Poster : "https://via.placeholder.com/400"} 
+                        src={ imgSrc }
+                        alt={movie.Title} 
+                        onError={
+                            ()=>{
+                                setImgSrc("https://via.placeholder.com/400");
+                            }
+                        }
+                    />
                 </div>
                 <div>
                     <div>
